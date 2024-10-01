b11fa7e7f6d4aee344f49f37f4a55973d291758a
Yash Khaitan <yashkhaitan02@gmail.com>
2024-10-01 14:51:32 +0530

    Exposed the api to fetch all teachers

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -3,7 +3,9 @@ from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
+from core.models.teachers import Teacher
 from .schema import AssignmentSchema
+from ..teachers.schema import TeacherSchema
 
 principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
 
@@ -14,3 +16,11 @@ def list_graded_submitted_assignments(p):
     submitted_graded_assignments = Assignment.get_graded_submitted_assignments()
     submitted_graded_assignments_dump = AssignmentSchema().dump(submitted_graded_assignments, many=True)
     return APIResponse.respond(data=submitted_graded_assignments_dump)
+
+
+@principal_assignments_resources.route('/teachers', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def list_teachers(p):
+    teachers = Teacher.get_all()
+    teachers_dump = TeacherSchema().dump(teachers, many=True)
+    return APIResponse.respond(data=teachers_dump)
